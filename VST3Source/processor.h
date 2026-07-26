ed1f8ce57c3e2c3f9b6bcd5daf66a7ece9ecb136
Jaga <122459216+Jaga266@users.noreply.github.com>
2026-07-27 01:55:31 +0200

    Add lightweight DSP state to VST3 processor

--- VST3Source/processor.h
+++ VST3Source/processor.h
@@ -7,11 +7,16 @@ public:
     Processor();
     static Steinberg::FUnknown* createInstance(void*) { return (Steinberg::Vst::IAudioProcessor*) new Processor(); }
     Steinberg::tresult PLUGIN_API initialize(Steinberg::FUnknown*) override;
+    Steinberg::tresult PLUGIN_API setupProcessing(Steinberg::Vst::ProcessSetup&) override;
     Steinberg::tresult PLUGIN_API process(Steinberg::Vst::ProcessData&) override;
     Steinberg::tresult PLUGIN_API setState(Steinberg::IBStream*) override;
     Steinberg::tresult PLUGIN_API getState(Steinberg::IBStream*) override;
+
 private:
-    double sweep = 0.7, crunch = 0.15;
+    double sweep = 0.7;
+    double crunch = 0.15;
     bool bypass = false;
+    double sampleRate = 44100.0;
+    float filterState[2] { 0.0f, 0.0f };
 };
 }
