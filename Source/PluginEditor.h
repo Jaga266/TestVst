665aa77de8858785ed51b6c1a184798d36e5fa5b
Jaga <122459216+Jaga266@users.noreply.github.com>
2026-07-26 21:39:27 +0200

    Add plugin editor header

---
+++ Source/PluginEditor.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+class RumbleLookAndFeel final : public juce::LookAndFeel_V4
+{
+public:
+    void drawRotarySlider (juce::Graphics&, int, int, int, int, float,
+                           float, float, juce::Slider&) override;
+};
+
+class RumbleSweepAudioProcessorEditor final : public juce::AudioProcessorEditor
+{
+public:
+    explicit RumbleSweepAudioProcessorEditor (RumbleSweepAudioProcessor&);
+    ~RumbleSweepAudioProcessorEditor() override;
+
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+
+    RumbleSweepAudioProcessor& processor;
+    RumbleLookAndFeel lookAndFeel;
+    juce::Slider sweepSlider, crunchSlider;
+    juce::Label sweepLabel, crunchLabel;
+    std::unique_ptr<Attachment> sweepAttachment, crunchAttachment;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RumbleSweepAudioProcessorEditor)
+};
